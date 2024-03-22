2560b9e5155dac55e9fc06eb10175b866dad42a1
sergeeva.va <vik280108@gmail.com>
2024-03-22 18:12:26 +0300

    доделать

--- src/main/java/bank/ATM.java
+++ src/main/java/bank/ATM.java
@@ -1,4 +1,47 @@
 package bank;
 
+import java.util.Scanner;
+
 public class ATM {
+    public void start() {
+        showStartATMInfo();
+
+        checkUserChoice();
+    }
+
+    private void checkUserChoice() {
+        String choice = getScannerData();
+
+        switch (choice) {
+            case "1" -> askCardData();
+            case "2" -> askAdminData();
+            default -> showChoiceError();
+        }
+    }
+
+    private String getScannerData() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    private void showChoiceError() {
+    }
+
+    private void askAdminData() {
+    }
+
+    private void askCardData() {
+        System.out.println("Вставьте карту.");
+        String cardNumber = getScannerData();
+        checkPinCode(cardNumber);
+    }
+
+    private void checkPinCode(String cardNumber) {
+        for (int i=0; i < Bank.getAccounts().size(); i++) {
+
+        }
+    }
+
+    private void showStartATMInfo() {
+        System.out.println("Добро пожаловать!");
+    }
 }
